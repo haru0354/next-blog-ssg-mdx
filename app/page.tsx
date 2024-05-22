088570434ba50fda0e050505599d6952ea3a4f04
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-05-23 01:27:57 +0900

    header内にglobalMenuが入るように修正

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@ import TopNewArticle from "./component/topPage/TopNewArticle";
 import TopCategory from "./component/topPage/TopCategory";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
-import GlobalMenu from "./component/GlobalMenu";
 
 export default function Home() {
   return (
     <>
       <Header isTopPage={true}/>
-      <GlobalMenu/>
       <main className="pb-20">
         <TopNewArticle />
         <TopCategory />
