8f6315006f2c2ad40a583ce1b0eac8fb0455279f
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-08-01 23:45:55 +0900

    テキストを太く変更できるように変更

--- app/component/designComponents/TextColor.tsx
+++ app/component/designComponents/TextColor.tsx
@@ -1,15 +1,24 @@
 type TextColorProps = {
-  color: "red" | "green";
+  color?: "red" | "green";
+  font?: "semibold" | "bold";
   children: React.ReactNode;
 };
 
-const TextColor: React.FC<TextColorProps> = ({ color , children }) => {
+const TextColor: React.FC<TextColorProps> = ({ color, font, children }) => {
   const colors = {
     red: "text-red-500",
-    green: "text-green-500"
-  }
+    green: "text-green-500",
+  };
 
-  return <p className={`${colors[color]}`}>{children}</p>;
+  const fonts = {
+    semibold: "font-semibold",
+    bold: "font-bold",
+  };
+
+  const colorClass = color ? colors[color] : "";
+  const fontClass = font ? fonts[font] : "";
+
+  return <p className={`${colorClass} ${fontClass}`}>{children}</p>;
 };
 
 export default TextColor;
