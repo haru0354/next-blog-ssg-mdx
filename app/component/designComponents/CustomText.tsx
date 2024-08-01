6e49d767d28204df23ee310f88a482b17d704644
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-08-02 02:37:37 +0900

    CustomTextにアンダーマーカーを追加

--- app/component/designComponents/CustomText.tsx
+++ app/component/designComponents/CustomText.tsx
@@ -2,10 +2,17 @@ type CustomTextProps = {
   color?: "red" | "green";
   font?: "semibold" | "bold";
   marker?: "red" | "green";
+  underMarker?: "red" | "green";
   children: React.ReactNode;
 };
 
-const CustomText: React.FC<CustomTextProps> = ({ color, font, marker, children }) => {
+const CustomText: React.FC<CustomTextProps> = ({
+  color,
+  font,
+  marker,
+  underMarker,
+  children,
+}) => {
   const colors = {
     red: "text-red-500",
     green: "text-green-500",
@@ -21,12 +28,23 @@ const CustomText: React.FC<CustomTextProps> = ({ color, font, marker, children }
     green: "bg-green-200",
   };
 
+  const underMarkers = {
+    red: "bg-red-underMarker",
+    green: "bg-green-underMarker",
+  };
+
   const colorClass = color ? colors[color] : "";
   const fontClass = font ? fonts[font] : "";
   const markerClass = marker ? markers[marker] : "";
+  const underMarkerClass = underMarker ? underMarkers[underMarker] : "";
 
-
-  return <span className={`${colorClass} ${fontClass} ${markerClass}`}>{children}</span>;
+  return (
+    <span
+      className={`${colorClass} ${fontClass} ${markerClass} ${underMarkerClass}`}
+    >
+      {children}
+    </span>
+  );
 };
 
 export default CustomText;
