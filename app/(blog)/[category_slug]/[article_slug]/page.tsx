173f975e4e213983e1bf49f707313f6fdfbfd92b
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-08-03 14:13:41 +0900

    記事の個別ページにmdxファイルに記載がなければ画像を表示しない

--- app/(blog)/[category_slug]/[article_slug]/page.tsx
+++ app/(blog)/[category_slug]/[article_slug]/page.tsx
@@ -52,13 +52,16 @@ const Page = async ({
         <h1 className="text-2xl font-semibold mx-2 my-4">
           {article.frontmatter.title}
         </h1>
-        <Image
-          src={`/image_webp/${article.frontmatter.eyeCatchName}.webp`}
-          alt={`${article.frontmatter.eyeCatchAlt}`}
-          width={750}
-          height={493}
-          className="mx-auto"
-        />
+        {article.frontmatter.eyeCatchAlt &&
+          article.frontmatter.eyeCatchName && (
+            <Image
+              src={`/image_webp/${article.frontmatter.eyeCatchName}.webp`}
+              alt={`${article.frontmatter.eyeCatchAlt}`}
+              width={750}
+              height={493}
+              className="mx-auto"
+            />
+          )}
         <p className="my-2 mx-2 mb-6 text-gray-600">
           投稿日：{article.frontmatter.date}
         </p>
