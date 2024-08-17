b1c9da56634c8f1a74bfc1c3a1cfa7062e520924
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-08-17 20:47:03 +0900

    ２階層名のカテゴリの一覧を取得する関数の作成

--- app/component/lib/CategoryService.ts
+++ app/component/lib/CategoryService.ts
@@ -31,6 +31,36 @@ export async function getMainCategories() {
   return mainCategories;
 }
 
+export async function getSecondCategories(firstLevelArticle_slug: string) {
+  const secondCategoriesDirectory = path.join(
+    process.cwd(),
+    "mdFile",
+    "category",
+    firstLevelArticle_slug,
+  );
+
+  const fileNames = fs.readdirSync(secondCategoriesDirectory);
+
+  const mdxFileNames = fileNames.filter((fileName) =>
+    fileName.endsWith(".mdx")
+  );
+
+  const secondCategories = await Promise.all(
+    mdxFileNames.map(async (mdxFileName) => {
+      const filePath = path.join(secondCategoriesDirectory, `${mdxFileName}`);
+      const fileContents = await fs.promises.readFile(filePath, "utf8");
+      const { data } = matter(fileContents);
+
+      return {
+        slug: mdxFileName.replace(".mdx", ""),
+        frontmatter: data,
+      };
+    })
+  );
+
+  return secondCategories;
+}
+
 export async function getCategory(params: string) {
   const slug = params;
   const filePath = path.join(
