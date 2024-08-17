ffbe8188b9e71b547b1ddbbd6561e58404da5db6
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-08-17 21:19:04 +0900

    getFirstLevelArticlesの修正

--- app/component/lib/FirstLevelArticleService.ts
+++ app/component/lib/FirstLevelArticleService.ts
@@ -1,57 +1,14 @@
 import path from "path";
 import fs from "fs";
 import matter from "gray-matter";
+import { getFixedPages } from "./FixedPageService";
+import { getMainCategories } from "./CategoryService";
 
 export async function getFirstLevelArticles() {
-  const articleDirectory = path.join(process.cwd(), "mdFile", "article");
-  const articleFileNames = fs.readdirSync(articleDirectory);
-  const mdxArticleFileNames = articleFileNames.filter((fileName) =>
-    fileName.endsWith(".mdx")
-  );
+  const fixedPages = await getFixedPages()
+  const categories = await getMainCategories()
 
-  const categoryDirectory = path.join(process.cwd(), "mdFile", "category");
-  const categoryFileNames = fs.readdirSync(categoryDirectory);
-  const mdxCategoryFileNames = categoryFileNames.filter((fileName) =>
-    fileName.endsWith(".mdx")
-  );
-
-  const articles = await Promise.all(
-    mdxArticleFileNames.map(async (mdxArticleFileName) => {
-      const filePath = path.join(articleDirectory, `${mdxArticleFileName}`);
-      try {
-        const fileContents = await fs.promises.readFile(filePath, "utf8");
-        const { data } = matter(fileContents);
-
-        return {
-          slug: mdxArticleFileName.replace(".mdx", ""),
-          frontmatter: data,
-        };
-      } catch (error) {
-        console.error(`${mdxArticleFileName}のファイルを読み取れませんでした`, error);
-        return null;
-      }
-    })
-  );
-
-  const categories = await Promise.all(
-    mdxCategoryFileNames.map(async (mdxCategoryFileName) => {
-      const filePath = path.join(categoryDirectory, `${mdxCategoryFileName}`);
-      try {
-        const fileContents = await fs.promises.readFile(filePath, "utf8");
-        const { data } = matter(fileContents);
-
-        return {
-          slug: mdxCategoryFileName.replace(".mdx", ""),
-          frontmatter: data,
-        };
-      } catch (error) {
-        console.error(`${mdxCategoryFileName}のファイルを読み取れませんでした`, error);
-        return null;
-      }
-    })
-  );
-
-  const FirstLevelArticles = [...articles, ...categories];
+  const FirstLevelArticles = [...fixedPages, ...categories];
 
   return FirstLevelArticles;
 }
@@ -90,6 +47,7 @@ export async function getFirstLevelArticle(firstLevelArticle_slug: string) {
       frontmatter: data,
       content,
     };
+    
   } catch (error) {
     console.error(
       `${firstLevelArticle_slug}.mdxのファイルを読み取れませんでした`,
