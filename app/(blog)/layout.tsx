df964d24685060ba2daf60daa6db101a2e768c2f
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-08-21 02:56:13 +0900

    子カテゴリコンポーネント適用によるレイアウトの修正

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -12,10 +12,7 @@ export default function RootLayout({
       <Header />
       <main className="flex justify-center mb-20">
         <div className="max-w-[1150px] flex flex-wrap justify-center">
-          <div className="flex flex-col flex-wrap w-full md:max-w-[800px] md:min-w-[800px]  md:mr-6">
             {children}
-          </div>
-          <SideMenu/>
         </div>
       </main>
       <Footer />
