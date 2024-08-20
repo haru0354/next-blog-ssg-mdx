eb6f3584705cba3501c0dc1841f8af2309ec50da
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-08-20 17:47:48 +0900

    3階層目のmetaデータの作成

--- app/(blog)/[firstLevelArticle_slug]/[secondLevelArticle_slug]/[thirdLevelArticle_slug]/page.tsx
+++ app/(blog)/[firstLevelArticle_slug]/[secondLevelArticle_slug]/[thirdLevelArticle_slug]/page.tsx
@@ -1,90 +1,129 @@
+import { Metadata } from "next";
 import Image from "next/image";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import rehypeSlug from "rehype-slug";
 import remarkToc from "remark-toc";
 import { useMDXComponents } from "@/mdx-components";
-import { getThirdLevelArticle, getThirdLevelArticles } from "@/app/component/lib/ThirdLevelArticleService";
+import {
+  getThirdLevelArticle,
+  getThirdLevelArticles,
+} from "@/app/component/lib/ThirdLevelArticleService";
 import Breadcrumbs from "@/app/component/contentArea/Breadcrumbs";
 import ArticleInArticleList from "@/app/component/contentArea/ArticleInArticleList";
 import CategoryInArticlesList2Images from "@/app/component/contentArea/CategoryInArticlesList2Images";
 import NotFound from "@/app/not-found";
 
-export async function generateStaticParams() {
-    const articles = await getThirdLevelArticles();
-  
-    return articles.map((article) => ({
-      firstLevelArticle_slug: article.parentCategorySlug,
-      secondLevelArticle_slug: article.childCategorySlug,
-      thirdLevelArticle_slug: article.slug,
-    }));
+export const generateMetadata = async ({
+  params,
+}: {
+  params: {
+    firstLevelArticle_slug: string;
+    secondLevelArticle_slug: string;
+    thirdLevelArticle_slug: string;
+  };
+}): Promise<Metadata> => {
+  const article = await getThirdLevelArticle(
+    params.firstLevelArticle_slug,
+    params.secondLevelArticle_slug,
+    params.thirdLevelArticle_slug
+  );
+
+  if (!article) {
+    return {
+      title: "記事がありません",
+      description: "記事がありません",
+    };
   }
 
-  const Page = async ({
-    params,
-  }: {
-    params: { firstLevelArticle_slug: string; secondLevelArticle_slug: string; thirdLevelArticle_slug: string; };
-  }) => {
-    const article = await getThirdLevelArticle(
-      params.firstLevelArticle_slug,
-      params.secondLevelArticle_slug,
-      params.thirdLevelArticle_slug,
-    );
-    const components = useMDXComponents();
-  
-    if (article === null) {
-      return <NotFound />;
-    }
-  
-    return (
-      <>
-        <div className="content-style p-4 bg-white border border-gray-200">
-          <Breadcrumbs
-            categorySlug={params.firstLevelArticle_slug}
-            categoryName={article.categoryName}
-          />
-          <h1 className="text-2xl font-semibold mx-2 my-4">
-            {article.frontmatter.title}
-          </h1>
-          {article.frontmatter.eyeCatchAlt &&
-            article.frontmatter.eyeCatchName && (
-              <Image
-                src={`/image_webp/${article.frontmatter.eyeCatchName}.webp`}
-                alt={`${article.frontmatter.eyeCatchAlt}`}
-                width={750}
-                height={493}
-                className="mx-auto mb-6"
-              />
-            )}
-          {article.frontmatter.date && (
-            <p className="mx-2 mb-6 text-gray-600 font-sm">
-              投稿日：{article.frontmatter.date}
-            </p>
-          )}
-          <MDXRemote
-            source={article.content}
-            components={components}
-            options={{
-              mdxOptions: {
-                remarkPlugins: [[remarkToc, { maxDepth: 3, heading: "目次" }]],
-                rehypePlugins: [rehypeSlug],
-              },
-            }}
-          />
-        </div>
-        {article.frontmatter.categoryName ? (
-          <CategoryInArticlesList2Images
-            params={params.firstLevelArticle_slug}
-            categoryName={article.frontmatter.categoryName}
-          />
-        ) : (
-          <ArticleInArticleList
-            categorySlug={params.firstLevelArticle_slug}
-            articleSlug={params.secondLevelArticle_slug}
-          />
-        )}
-      </>
-    );
+  return {
+    title: article.frontmatter.title,
+    description: article.frontmatter.description,
+    openGraph: {
+      title: article.frontmatter.title,
+      description: article.frontmatter.description,
+    },
   };
-  
-  export default Page;
-  
+};
+
+export async function generateStaticParams() {
+  const articles = await getThirdLevelArticles();
+
+  return articles.map((article) => ({
+    firstLevelArticle_slug: article.parentCategorySlug,
+    secondLevelArticle_slug: article.childCategorySlug,
+    thirdLevelArticle_slug: article.slug,
+  }));
+}
+
+const Page = async ({
+  params,
+}: {
+  params: {
+    firstLevelArticle_slug: string;
+    secondLevelArticle_slug: string;
+    thirdLevelArticle_slug: string;
+  };
+}) => {
+  const article = await getThirdLevelArticle(
+    params.firstLevelArticle_slug,
+    params.secondLevelArticle_slug,
+    params.thirdLevelArticle_slug
+  );
+  const components = useMDXComponents();
+
+  if (article === null) {
+    return <NotFound />;
+  }
+
+  return (
+    <>
+      <div className="content-style p-4 bg-white border border-gray-200">
+        <Breadcrumbs
+          categorySlug={params.firstLevelArticle_slug}
+          categoryName={article.categoryName}
+        />
+        <h1 className="text-2xl font-semibold mx-2 my-4">
+          {article.frontmatter.title}
+        </h1>
+        {article.frontmatter.eyeCatchAlt &&
+          article.frontmatter.eyeCatchName && (
+            <Image
+              src={`/image_webp/${article.frontmatter.eyeCatchName}.webp`}
+              alt={`${article.frontmatter.eyeCatchAlt}`}
+              width={750}
+              height={493}
+              className="mx-auto mb-6"
+            />
+          )}
+        {article.frontmatter.date && (
+          <p className="mx-2 mb-6 text-gray-600 font-sm">
+            投稿日：{article.frontmatter.date}
+          </p>
+        )}
+        <MDXRemote
+          source={article.content}
+          components={components}
+          options={{
+            mdxOptions: {
+              remarkPlugins: [[remarkToc, { maxDepth: 3, heading: "目次" }]],
+              rehypePlugins: [rehypeSlug],
+            },
+          }}
+        />
+      </div>
+      {article.frontmatter.categoryName ? (
+        <CategoryInArticlesList2Images
+          params={params.firstLevelArticle_slug}
+          categoryName={article.frontmatter.categoryName}
+        />
+      ) : (
+        <ArticleInArticleList
+          categorySlug={params.firstLevelArticle_slug}
+          articleSlug={params.secondLevelArticle_slug}
+        />
+      )}
+    </>
+  );
+};
+
+export default Page;
