a9745438707b9ef42e7b4533ee14a0fdb293d411
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-08-22 15:28:51 +0900

    sitemap.xmlに固定ページを追加

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,15 @@ import TopNewArticle from "./component/topPage/TopNewArticle";
 import TopCategory from "./component/topPage/TopCategory";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+import { getAllArticles } from "./component/lib/AllArticleService";
+import { getFixedPages } from "./component/lib/FixedPageService";
 
 export default async function Home() {
+  const allArticles = await getAllArticles()
+  const fixedPages = await getFixedPages()
+
+  console.log(fixedPages);
+  
   return (
     <>
       <Header isTopPage={true} />
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,13 @@
 import { MetadataRoute } from "next";
 import { getFirstLevelArticles } from "./component/lib/FirstLevelArticleService";
 import { getSecondLevelArticles } from "./component/lib/SecondLevelArticleService";
+import { getFixedPages } from "./component/lib/FixedPageService";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseURL = "サイトのURL";
   const _lastModified = new Date();
 
+  const fixedPages = await getFixedPages();
   const secondLevelArticles = await getSecondLevelArticles();
   const firstLevelArticles = await getFirstLevelArticles();
 
@@ -20,19 +22,35 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     },
   ];
 
-  const dynamicPathsSecondLevelArticle = secondLevelArticles.map((secondLevelArticle) => {
+  const dynamicPathsFixedArticle = fixedPages.map((fixedPage) => {
     return {
-      url: `${baseURL}/${secondLevelArticle.categorySlug}/${secondLevelArticle.slug}`,
-      lastModified: new Date(secondLevelArticle.frontmatter.date),
+      url: `${baseURL}/${fixedPage?.slug}`,
+      lastModified: new Date(fixedPage?.frontmatter.date),
     };
   });
 
-  const dynamicPathsFirstLevelArticles = firstLevelArticles.map((firstLevelArticle) => {
-    return {
-      url: `${baseURL}/${firstLevelArticle?.slug}`,
-      lastModified: new Date(firstLevelArticle?.frontmatter.date),
-    };
-  });
+  const dynamicPathsSecondLevelArticle = secondLevelArticles.map(
+    (secondLevelArticle) => {
+      return {
+        url: `${baseURL}/${secondLevelArticle.categorySlug}/${secondLevelArticle.slug}`,
+        lastModified: new Date(secondLevelArticle.frontmatter.date),
+      };
+    }
+  );
 
-  return [...staticPaths, ...dynamicPathsSecondLevelArticle, ...dynamicPathsFirstLevelArticles];
+  const dynamicPathsFirstLevelArticles = firstLevelArticles.map(
+    (firstLevelArticle) => {
+      return {
+        url: `${baseURL}/${firstLevelArticle?.slug}`,
+        lastModified: new Date(firstLevelArticle?.frontmatter.date),
+      };
+    }
+  );
+
+  return [
+    ...staticPaths,
+    ...dynamicPathsFixedArticle,
+    ...dynamicPathsSecondLevelArticle,
+    ...dynamicPathsFirstLevelArticles,
+  ];
 }
