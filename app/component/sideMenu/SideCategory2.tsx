6083768c890b3d521a0e0f408a55543713d6da0b
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-09-06 19:42:03 +0900

    サイドバーの親カテゴリが任意の項目を表示できるよう変更

--- app/component/sideMenu/SideCategory2.tsx
+++ app/component/sideMenu/SideCategory2.tsx
@@ -1,8 +1,32 @@
 import Link from "next/link";
 import { getParentCategories } from "../lib/CategoryService";
+import { getSideCategoriesMenu } from "../lib/MenuService";
+
+type Category = {
+  slug: string;
+  categoryName: string;
+};
 
 const SideCategory2 = async () => {
-  const categories = await getParentCategories();
+  const sideCategoriesMenu = await getSideCategoriesMenu();
+
+  if (!sideCategoriesMenu || !sideCategoriesMenu.items) {
+    return null;
+  }
+
+  let categories;
+  
+  if (sideCategoriesMenu.display === false) {
+    const parentCategories = await getParentCategories();
+    categories = parentCategories.map((category) => {
+      return {
+        slug: category?.slug,
+        categoryName: category?.frontmatter.categoryName,
+      };
+    });
+  } else {
+    categories = sideCategoriesMenu?.items;
+  }
 
   if (!categories) {
     return null;
@@ -12,14 +36,14 @@ const SideCategory2 = async () => {
     <nav className="bg-white border-r border-l mb-8 border-gray-500">
       <h3 className="w-full p-4 bg-main-gray text-white font-bold">カテゴリ</h3>
       <ul>
-        {categories.map((category) => {
+        {categories.map((category: Category) => {
           return (
             <Link href={`/${category?.slug}`} key={category?.slug}>
               <li
                 className="p-4 border-b border-gray-500 hover:bg-hover-blue"
-                key={category?.frontmatter.categoryName}
+                key={category?.categoryName}
               >
-                {category?.frontmatter.categoryName}
+                {category?.categoryName}
               </li>
             </Link>
           );
