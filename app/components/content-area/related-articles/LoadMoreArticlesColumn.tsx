a23d828ed6c41a5a3c893ebe944c3ac3c4118058
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-11-09 11:26:08 +0900

    LoadMoreArticlesColumnのディレクトリ移動

--- app/components/content-area/related-articles/LoadMoreArticlesColumn.tsx
+++ app/components/content-area/related-articles/LoadMoreArticlesColumn.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Button from "../ui/Button";
+import Button from "../../ui/Button";
 
 type LoadMoreArticlesColumnProps = {
   articles: Article[];
