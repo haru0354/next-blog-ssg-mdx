cb009032cd9d6af1b66c76385a702c489c92efe6
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-11-19 19:18:41 +0900

    SideRecommendArticlesの記述方法を分かりやすい用リファクタリング

--- app/components/side-menu/parts/SideRecommendArticles.tsx
+++ app/components/side-menu/parts/SideRecommendArticles.tsx
@@ -21,31 +21,31 @@ const SideRecommendArticles: React.FC<SideRecommendArticlesProps> = async ({
     return null;
   }
 
+  if (recommendArticles.frontmatter.display === false) {
+    return null;
+  }
+
   const h3BorderDesign = border ? "w-[300px]" : "mb-4 rounded";
 
   return (
-    <>
-      {recommendArticles.frontmatter.display && (
-        <div className="w-full">
-          <h3
-            className={`p-4 font-bold text-white bg-layout-mainColor  ${h3BorderDesign}`}
-          >
-            おすすめの記事
-          </h3>
-          {recommendArticles.frontmatter.images.map((image: Image) => (
-            <Link href={`/${image.url}`}>
-              <Image
-                src={`/thumbnail_webp/${image.eyeCatchName}.webp`}
-                alt={image.eyeCatchAlt}
-                width={300}
-                height={196}
-                className="mx-auto md:mt-0 my-8"
-              />
-            </Link>
-          ))}
-        </div>
-      )}
-    </>
+    <div className="w-full">
+      <h3
+        className={`p-4 font-bold text-white bg-layout-mainColor  ${h3BorderDesign}`}
+      >
+        おすすめの記事
+      </h3>
+      {recommendArticles.frontmatter.images.map((image: Image) => (
+        <Link href={`/${image.url}`}>
+          <Image
+            src={`/thumbnail_webp/${image.eyeCatchName}.webp`}
+            alt={image.eyeCatchAlt}
+            width={300}
+            height={196}
+            className="mx-auto md:mt-0 my-8"
+          />
+        </Link>
+      ))}
+    </div>
   );
 };
 
