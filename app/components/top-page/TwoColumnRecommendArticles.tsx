3bf121561eeeba29dc3be64d4131a7bbfb9b438d
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-03-17 17:59:12 +0900

    topPageのおすすめ記事のボタンリンクを作成したコンポーネントに変更

--- app/components/top-page/TwoColumnRecommendArticles.tsx
+++ app/components/top-page/TwoColumnRecommendArticles.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import Link from "next/link";
+
 import { getTwoColumnRecommendArticles } from "@/app/lib/service/topPageService";
-import Button from "../ui/Button";
+import NextLinkButton from "../ui/NextLinkButton";
 
 const TwoColumnRecommendArticles = async () => {
   const twoColumnRecommendArticles = await getTwoColumnRecommendArticles();
@@ -53,9 +53,15 @@ const TwoColumnRecommendArticles = async () => {
                             )}...`
                           : article?.frontmatter.description}
                       </p>
-                      <Link href={`/${article?.slug}`} key={article?.slug}>
-                        <Button className="mx-auto block">記事ページへ</Button>
-                      </Link>
+                      <NextLinkButton
+                        className="mx-auto block"
+                        size="normal"
+                        color="gray"
+                        href={`/${article?.slug}`}
+                        key={article?.slug}
+                      >
+                        記事ページへ
+                      </NextLinkButton>
                     </div>
                   </div>
                 </div>
