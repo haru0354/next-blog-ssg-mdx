57bf114a4aadf5f678df7c22e1381956fa34f4d7
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-06-02 10:43:41 +0900

    topのlayoutファイルに共通コンポーネントがくるよう修正

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,6 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import { useMDXComponents } from "@/mdx-components";
 
 import { getTopPageArticle } from "./lib/service/topPageService";
-import Header from "./components/Header";
-import GlobalMenu from "./components/GlobalMenu";
-import Footer from "./components/Footer";
 import SideMenu from "./components/side-menu/SideMenu";
 import TopNewArticle from "./components/top-page/TopNewArticle";
 import TopCategory from "./components/top-page/TopCategory";
@@ -24,64 +21,54 @@ export default async function Home() {
   return (
     <>
       {topPageArticle?.frontmatter.contentPage ? (
-        <>
-          <Header />
-          <GlobalMenu />
-          <MainLayout>
-            <LeftColumn>
-              <div className="content-style p-4">
-                <h1 className="text-2xl font-semibold mx-2 my-4">
-                  {topPageArticle.frontmatter.title}
-                </h1>
-                {topPageArticle.content &&
-                  topPageArticle.frontmatter.eyeCatchName && (
-                    <Image
-                      src={`/image_webp/${topPageArticle.frontmatter.eyeCatchName}.webp`}
-                      alt={`${topPageArticle.frontmatter.eyeCatchAlt}`}
-                      width={750}
-                      height={493}
-                      className="mx-auto mb-6"
-                    />
-                  )}
-                {topPageArticle.content && (
-                  <>
-                    {topPageArticle.frontmatter.date && (
-                      <p className="mx-2 mb-6 text-gray-600 font-sm">
-                        投稿日：{topPageArticle.frontmatter.date}
-                      </p>
-                    )}
-                    <MDXRemote
-                      source={topPageArticle.content}
-                      components={components}
-                      options={{
-                        mdxOptions: {
-                          remarkPlugins: [
-                            [remarkToc, { maxDepth: 3, heading: "目次" }],
-                          ],
-                          rehypePlugins: [rehypeSlug],
-                        },
-                      }}
-                    />
-                  </>
+        <MainLayout>
+          <LeftColumn>
+            <div className="content-style p-4">
+              <h1 className="text-2xl font-semibold mx-2 my-4">
+                {topPageArticle.frontmatter.title}
+              </h1>
+              {topPageArticle.content &&
+                topPageArticle.frontmatter.eyeCatchName && (
+                  <Image
+                    src={`/image_webp/${topPageArticle.frontmatter.eyeCatchName}.webp`}
+                    alt={`${topPageArticle.frontmatter.eyeCatchAlt}`}
+                    width={750}
+                    height={493}
+                    className="mx-auto mb-6"
+                  />
                 )}
-              </div>
-            </LeftColumn>
-            <SideMenu />
-          </MainLayout>
-          <Footer />
-        </>
+              {topPageArticle.content && (
+                <>
+                  {topPageArticle.frontmatter.date && (
+                    <p className="mx-2 mb-6 text-gray-600 font-sm">
+                      投稿日：{topPageArticle.frontmatter.date}
+                    </p>
+                  )}
+                  <MDXRemote
+                    source={topPageArticle.content}
+                    components={components}
+                    options={{
+                      mdxOptions: {
+                        remarkPlugins: [
+                          [remarkToc, { maxDepth: 3, heading: "目次" }],
+                        ],
+                        rehypePlugins: [rehypeSlug],
+                      },
+                    }}
+                  />
+                </>
+              )}
+            </div>
+          </LeftColumn>
+          <SideMenu />
+        </MainLayout>
       ) : (
-        <>
-          <Header isTopPage={true} />
-          <GlobalMenu />
-          <main>
-            <TwoColumnRecommendArticles />
-            <TopRecommendArticles />
-            <TopNewArticle />
-            <TopCategory />
-          </main>
-          <Footer />
-        </>
+        <main>
+          <TwoColumnRecommendArticles />
+          <TopNewArticle />
+          <TopCategory />
+          <TopRecommendArticles />
+        </main>
       )}
     </>
   );
